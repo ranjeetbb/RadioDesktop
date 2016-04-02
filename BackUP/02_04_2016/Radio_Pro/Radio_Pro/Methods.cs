b95ecd7c3f64d90f4ebfa6c8d8758fb741f20c20
Ranjeet Bhardwaj <ranjeet@tekticks.co.in>
2016-04-02 18:58:05 +0530

    Committed on 02/04/2016
    New Update with changes

---
+++ BackUP/02_04_2016/Radio_Pro/Radio_Pro/Methods.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+namespace Radio_Pro
+{
+    class Methods
+    {
+        XCrypt.XCryptEngine xe = new XCrypt.XCryptEngine(XCrypt.XCryptEngine.AlgorithmType.Rijndael);
+
+        public string GetCreateTableSql(string table, ref MySqlCommand cmd)
+        {
+            cmd.CommandText = "SHOW CREATE TABLE `" + table + "`;";
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da = null;
+            return dt.Rows[0][1].ToString().Replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS") + ";";
+        }
+
+        public string[] GetColumnNames(string table, ref MySqlCommand cmd)
+        {
+            cmd.CommandText = "SHOW COLUMNS FROM `" + table + "`;";
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da = null;
+            string[] sa = new string[dt.Rows.Count];
+            int count = -1;
+            foreach (DataRow dr in dt.Rows)
+            {
+                count += 1;
+
+                sa[count] = dr[0].ToString();
+            }
+            return sa;
+        }
+
+        public string[] GetTableNames(ref MySqlCommand cmd)
+        {
+            cmd.CommandText = "SHOW TABLES;";
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da = null;
+            string[] sa = new string[dt.Rows.Count];
+            int count = -1;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                count += 1;
+                sa[count] = dr[0] + "";
+            }
+
+            return sa;
+        }
+
+        public string GetServerVersion(ref MySqlCommand cmd)
+        {
+            cmd.CommandText = "SHOW variables WHERE Variable_name = 'version';";
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt.Rows[0][1].ToString();
+        }
+
+        public long GetServerMaxAllowedPacket(ref MySqlCommand cmd)
+        {
+            cmd.CommandText = "SHOW variables WHERE Variable_name = 'max_allowed_packet';";
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return Convert.ToInt64(dt.Rows[0][1]);
+        }
+
+        public string GetDatabaseName(string ConnectionString)
+        {
+            string[] sa = ConnectionString.Split(';');
+            foreach (string s in sa)
+            {
+                if (s.ToLower().StartsWith("database"))
+                {
+                    string[] sb = s.Split('=');
+                    return sb[1];
+                }
+            }
+            throw new Exception("Database Name is not detected in Connection String.");
+        }
+
+        public string GetBLOBSqlDataStringFromBytes(byte[] ba)
+        {
+            // Method 1 (slower)
+            //return "0x"+ BitConverter.ToString(bytes).Replace("-", string.Empty);
+
+            // Method 2 (faster)
+            char[] c = new char[ba.Length * 2 + 2];
+            byte b;
+            c[0] = '0'; c[1] = 'x';
+            for (int y = 0, x = 2; y < ba.Length; ++y, ++x)
+            {
+                b = ((byte)(ba[y] >> 4));
+                c[x] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+                b = ((byte)(ba[y] & 0xF));
+                c[++x] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+            }
+            return new string(c);
+        }
+
+        public string Encrypt(string input, string key, int saltSize)
+        {
+            string salt = Salt(saltSize);
+            return salt + xe.Encrypt(input, key + salt);
+        }
+
+        public string Decrypt(string input, string key, int saltSize)
+        {
+            try
+            {
+                string salt = input.Substring(0, saltSize);
+                string data = input.Substring(saltSize);
+                return xe.Decrypt(data, key + salt);
+            }
+            catch
+            {
+                string a = input;
+                throw new Exception("Invalid Key.");
+            }
+        }
+
+        public Random random = new Random((int)DateTime.Now.Ticks);
+
+        public string RandomString(int size)
+        {
+            StringBuilder sb = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                sb.Append(ch);
+            }
+            return sb.ToString();
+        }
+
+        public int GetSaltSize(string key)
+        {
+            int a = key.GetHashCode();
+            string b = Convert.ToString(a);
+            char[] ca = b.ToCharArray();
+            int c = 0;
+            foreach (char cc in ca)
+                c += Convert.ToInt32(cc.ToString());
+            return c;
+        }
+
+        public string Salt(int saltSize)
+        {
+            string op = xe.Encrypt(RandomString(30)).Substring(0, saltSize);
+            return op;
+        }
+
+        public string Sha1Hash(string input)
+        {
+            SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider();
+            byte[] ba = Encoding.UTF8.GetBytes(input);
+            byte[] ba2 = sha.ComputeHash(ba);
+            return BitConverter.ToString(ba2).Replace("-", string.Empty).ToLower();
+        }
+    }
+}
---
+++ BackUP/02_04_2016/Radio_Pro/Radio_Pro/frmMangeUsers.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Radio_Pro
+{
+    partial class frmMangeUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbDeviceId = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCreateAccount.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCreateAccount.FlatAppearance.BorderColor = System.Drawing.SystemColors.InfoText;
+            this.btnCreateAccount.FlatAppearance.BorderSize = 0;
+            this.btnCreateAccount.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCreateAccount.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCreateAccount.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreateAccount.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCreateAccount.Location = new System.Drawing.Point(33, 214);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(200, 29);
+            this.btnCreateAccount.TabIndex = 0;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = false;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUserName.Location = new System.Drawing.Point(33, 53);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(200, 26);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtContact.Location = new System.Drawing.Point(33, 112);
+            this.txtContact.MaxLength = 10;
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(200, 26);
+            this.txtContact.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contact";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Device Id";
+            // 
+            // cmbDeviceId
+            // 
+            this.cmbDeviceId.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbDeviceId.FormattingEnabled = true;
+            this.cmbDeviceId.Location = new System.Drawing.Point(33, 172);
+            this.cmbDeviceId.Name = "cmbDeviceId";
+            this.cmbDeviceId.Size = new System.Drawing.Size(200, 26);
+            this.cmbDeviceId.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Error";
+            // 
+            // frmMangeUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(271, 293);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbDeviceId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMangeUsers";
+            this.Text = "MangeUsers";
+            this.Load += new System.EventHandler(this.frmMangeUsers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbDeviceId;
+        private System.Windows.Forms.Label label4;
+    }
+}
